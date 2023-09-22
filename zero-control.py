d6ec9432adb8b76d7ba521047e42592d4417c4ce
Parag Patil <parag.pat20@gmail.com>
2023-09-22 15:33:11 +0530

    Hello

--- zero-control.py
+++ zero-control.py
@@ -92,7 +92,7 @@ def Client_Start(server_ip, server_port):
     print("Connected to the server")
 
     # Create a Drone instance
-    my_drone = Drone('/dev/serial1')
+    my_drone = Drone('/dev/serial0')
 
     while True:
         # Send P dictionary values to the server
