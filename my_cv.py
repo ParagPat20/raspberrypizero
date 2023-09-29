3c3aaaec6c47cd5ea2eba2d0ccbbb7240becd225
Parag Patil <parag.pat20@gmail.com>
2023-09-29 11:21:42 +0530

    Hello

--- my_cv.py
+++ my_cv.py
@@ -8,10 +8,28 @@ import numpy as np
 stream = io.BytesIO()
 
 with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)  # Set the resolution of the video frames
+    camera.framerate = 24  # Set the frame rate of the video stream
+    camera.start_preview()  # Start the camera preview
+
+    # Wait for the camera to warm up
+    time.sleep(2)
+
     while True:
         camera.capture(stream, format='jpeg')
         # Construct a numpy array from the stream
-        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
         # "Decode" the image from the array, preserving color
         image = cv2.imdecode(data, 1)
-        cv2.imshow('frame', image)
+        if image is True:
+            print('yay')
+        # Check for the 'q' key to exit the live stream
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # Clear the stream in preparation for the next frame
+        stream.seek(0)
+        stream.truncate()
+
+    # Clean up
+    cv2.destroyAllWindows()
