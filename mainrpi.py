1da535565937cc9ad875bc7ba8a778051267a463
Parag Patil <parag.pat20@gmail.com>
2023-10-04 14:09:21 +0530

    Hello

--- mainrpi.py
+++ mainrpi.py
@@ -3,9 +3,12 @@ from pymavlink import mavutil
 import time
 import socket
 import threading
+import geopy
+import geopy.distance
+from geopy.distance import great_circle
 
-local_host = '192.168.22.122'
-remote_host = '192.168.22.122'
+local_host = '0.0.0.0'
+remote_host = '192.168.155.101'
 mode_port = 60001
 ctrl_port = 60003
 status_port = [60002,60004,60006,60008]
@@ -41,7 +44,7 @@ class Drone:
             mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
             0b0000111111111000, # type_mask (only positions enabled)
             north, east, down,
-            0, 0, 0, # x, y, z velocity in m/s  (not used)
+            0, 0, 0, # x, y, z velocixty in m/s  (not used)
             0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
             0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
         # send command to vehicle
@@ -65,7 +68,7 @@ class Drone:
                 break
             time.sleep(1)
 
-    def arm(self, mode):
+    def arm(self, mode='GUIDED'):
         print("Arming motors")
         self.vehicle.mode = VehicleMode(mode)
         self.vehicle.armed = True
@@ -116,7 +119,6 @@ print("CD3 Connected")
 Drone_ID = MCU
 
 def ServerSendStatus(drone, local_host, status_port):
-
     status_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     status_socket.bind((local_host, status_port))
     status_socket.listen(1)
@@ -124,13 +126,24 @@ def ServerSendStatus(drone, local_host, status_port):
     print('{} - ServerSendStatus is started!'.format(time.ctime()))
 
     while True:
-        client_connection, client_address = status_socket.accept()
+        try:
+            client_connection, client_address = status_socket.accept()
 
-        status_data = status(drone)  # Call your status function here
-        client_connection.send(status_data.encode())  # Send the string
+            status_data = status(drone)  # Call your status function here
+            client_connection.send(status_data.encode())  # Send the string
 
-        client_connection.close()
-        time.sleep(1)
+            client_connection.close()
+        except KeyboardInterrupt:
+            # Handle KeyboardInterrupt to gracefully exit the loop
+            break
+        except Exception as e:
+            # Handle other exceptions, e.g., if the client disconnects unexpectedly
+            print(f"Error: {e}")
+            time.sleep(1)
+        finally:
+            time.sleep(1)
+
+    status_socket.close()
 
 
 def status(drone):
@@ -187,45 +200,25 @@ def ServerRecvCmd(local_host):
             drone_land(CD3)
         if immediate_command_str == 'TakeOff':
             Drone_ID.takeoff()
-        if immediate_command_str == 'takeoff_all':
+        if immediate_command_str == 'takeoff':
             drone_takeoff(MCU)
             drone_takeoff(CD1)
             drone_takeoff(CD2)
             drone_takeoff(CD3)
-        if immediate_command_str == 'square':
-            drone_ctrl(MCU,1,0,0)
-            drone_ctrl(CD2,1,0,0)
-            print("Square1")
-            time.sleep(8)
-            drone_ctrl(MCU,0,0,0)
-            drone_ctrl(CD2,0,0,0)
-            print("Square2")
-            time.sleep(8)
-            drone_ctrl(MCU,-1,0,0)
-            drone_ctrl(CD2,-1,0,0)
-            print("Square3")
-            time.sleep(8)
-            drone_ctrl(MCU,0,0,0)
-            drone_ctrl(CD2,0,0,0)
-            print("Square4")
-            time.sleep(8)
         if immediate_command_str == 'POSHOLD':
             drone_mode(MCU,'POSHOLD')
             drone_mode(CD1,'POSHOLD')
             drone_mode(CD2,'POSHOLD')
             drone_mode(CD3,'POSHOLD')
-        if immediate_command_str == 'squarevel':
-            drone_vel_ctrl(MCU,-0.8,0,0,5)
-            drone_vel_ctrl(CD2,-0.8,0,0,5)
-            print("Square1")
-            time.sleep(8)
-            drone_vel_ctrl(MCU,0.8,0,0,5)
-            drone_vel_ctrl(CD2,0.8,0,0,5)
-            print("Square2")
-            time.sleep(8)
-
-            
-
+        if immediate_command_str == 'square':
+            print("Square")
+            square()
+        if immediate_command_str == 'line':
+            print("Line")
+            line()
+        if immediate_command_str == 'tri':
+            print("Triangle")
+            tri()
 
         client_connection.close()
 
@@ -248,10 +241,7 @@ def ServerRecvControl(local_host):
         
         try:
             x, y, z = map(float, control_command_str.split(','))  # Split and convert to floats
-            drone_vel_ctrl(MCU,x,y,z,1)
-            drone_vel_ctrl(CD1,x,y,z,1)
-            drone_vel_ctrl(CD2,x,y,z,1)
-            drone_vel_ctrl(CD3,x,y,z,1)
+            Drone_ID.send_ned_velocity(x, y, z, 1)
         except ValueError:
             print("Invalid control command format. Expected 'x,y,z'")
 
@@ -265,9 +255,10 @@ def drone_vel_ctrl(drone,x,y,z,t):
 
 def drone_takeoff(drone):
     threading.Thread(target=drone.takeoff).start()
+    print(drone, "takeoff")
 
 def drone_arm(drone):
-    threading.Thread(target=drone.arm, args=('GUIDED',)).start()
+    threading.Thread(target=drone.arm, args=('STABILIZE',)).start()
 
 def drone_land(drone):
     threading.Thread(target=drone.land).start()
@@ -283,6 +274,139 @@ def start_server_service(local_host):
     threading.Thread(target=ServerRecvControl, args=(local_host,)).start()
     print("Thread serverRecvMode and ServerRecvControl are started!")
 
+def goto(drone, lat, lon, alt, groundspeed = 1):
+    print('{} - Calling goto_gps_location_relative(lat={}, lon={}, alt={}, groundspeed={}).'.format(time.ctime(), lat, lon, alt, groundspeed))
+    destination = LocationGlobalRelative(lat, lon, alt)
+    current_lat = drone.vehicle.location.global_relative_frame.lat
+    current_lon = drone.vehicle.location.global_relative_frame.lon
+    current_alt = drone.vehicle.location.global_relative_frame.alt
+
+    while ((distance_between_two_gps_coord((current_lat,current_lon), (lat,lon)) >0.6) or (abs(current_alt-alt)>0.3)):
+        # Execute fly command.
+        drone.vehicle.simple_goto(destination)
+        # wait for one second.
+        time.sleep(0.5)
+        current_lat = drone.vehicle.location.global_relative_frame.lat
+        current_lon = drone.vehicle.location.global_relative_frame.lon
+        current_alt = drone.vehicle.location.global_relative_frame.alt
+        print('{} - Horizontal distance to destination: {} m.'.format(time.ctime(), distance_between_two_gps_coord((current_lat,current_lon), (lat,lon))))
+        print('{} - Perpendicular distance to destination: {} m.'.format(time.ctime(), current_alt-alt))
+    print('{} - After calling goto_gps_location_relative(), vehicle state is:'.format(time.ctime()))
+
+def new_coords(original_gps_coord, displacement, rotation_degree):
+    vincentyDistance = geopy.distance.distance(meters = displacement)
+    original_point = geopy.Point(original_gps_coord[0], original_gps_coord[1])
+    new_gps_coord = vincentyDistance.destination(point=original_point, bearing=rotation_degree)
+    new_gps_lat = new_gps_coord.latitude
+    new_gps_lon = new_gps_coord.longitude
+    # If convert float to decimal, round will be accurate, but will take 50% more time. Not necessary.
+    #new_gps_lat = decimal.Decimal(new_gps_lat)
+    #new_gps_lon = decimal.Decimal(new_gps_lon)
+    return (round(new_gps_lat, 7), round(new_gps_lon, 7))
+
+def distance_between_two_gps_coord(point1, point2):
+    distance = great_circle(point1, point2).meters
+    return distance
+
+def set_yaw(drone, yaw_inDegree):
+    print('\n')
+    print('{} - Calling function set_yaw(yaw_inDegree={}).'.format(time.ctime(), yaw_inDegree))
+    # Do not pass True of False into msg, just in case the conversion is unpredictable.
+
+    degreeToTurn = yaw_inDegree
+    if degreeToTurn > 180:
+        degreeToTurn = 360 - degreeToTurn
+        estimatedTime = degreeToTurn/30.0 + 1 # Upon testing, the turning speed is 30 degree/second. Add one more second.
+        print('{} - Absolute degree to turn is {} degree. Estimated time is {} seconds.'.format(time.ctime(), degreeToTurn, estimatedTime))
+    else:
+        is_relative = 0
+        print('{} - The target degree is absolute degree[0~360](0=North, 90=East).'.format(time.ctime()))
+        currentHeading = drone.vehicle.heading
+        print('{} - Current heading is {} degree.'.format(time.ctime(), currentHeading))
+        print('{} - Target heading is {} degree.'.format(time.ctime(), yaw_inDegree))
+        degreeToTurn = abs(yaw_inDegree - drone.vehicle.heading)
+        if degreeToTurn > 180:
+            degreeToTurn = 360 - degreeToTurn
+        estimatedTime = degreeToTurn/30.0 + 1 # Upon testing, the turning speed is 30 degree/second. Add one more second.
+        print('{} - Absolute degree to turn is {} degree. Estimated time is {} seconds.'.format(time.ctime(), degreeToTurn, estimatedTime))
+    
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = drone.vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        yaw_inDegree,  # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, if set to 0, yaw is an absolute direction[0-360](0=north, 90=east); if set to 1, yaw is a relative degree to the current yaw direction.
+        0, 0, 0)    # param 5 ~ 7 not used
+    
+    # Send MAVLink message.
+    drone.vehicle.send_mavlink(msg)
+
+def cu_lo(drone):
+    point = drone.vehicle.location.global_relative_frame
+    return point
+
+def line(dis = 4, alt = 2):
+    pointA = cu_lo(MCU)
+    cdis = 0
+    A = (pointA.lat, pointA.lon)
+    cdis = cdis + dis
+    B = new_coords(A,cdis,0)
+    cdis = cdis + dis
+    C = new_coords(A,cdis,0)
+    cdis = cdis + dis
+    D = new_coords(A,cdis,0)
+    set_yaw(MCU,0)
+    goto(CD1,B[0],B[1],alt)
+    set_yaw(CD1,0)
+    goto(CD2,C[0],C[1],alt)
+    set_yaw(CD2,0)
+    goto(CD3,D[0],D[1],alt)
+    set_yaw(CD3,0)
+    time.sleep(1)
+    
+    
+
+def square(side = 8, alt = 3):
+    pointA = cu_lo(MCU)
+    mcuculo = (pointA.lat,pointA.lon)
+    pointB = cu_lo(CD1)
+    cd1culo = (pointB.lat,pointB.lon)
+    pointC = cu_lo(CD2)
+    cd2culo = (pointC.lat,pointC.lon)
+    pointD = cu_lo(CD3)
+    cd3culo = (pointD.lat,pointD.lon)
+
+    cd1golo = new_coords(cd1culo, side, 90)
+    goto(CD1,cd1golo[0],cd1golo[1],alt)
+
+    cd3golo = new_coords(cd3culo, side, 90)
+    goto(CD3,cd3golo[0],cd3golo[1],alt)
+
+    cd1golo = new_coords(mcuculo, side, 90)
+    goto(CD1,cd1golo[0],cd1golo[1],alt)
+
+    cd3golo = new_coords(cd2culo, side, 90)
+    goto(CD3,cd3golo[0],cd3golo[1],alt)
+
+    set_yaw(MCU,0)
+    set_yaw(CD1,0)
+    set_yaw(CD2,0)
+    set_yaw(CD3,0)
+
+    time.sleep(2)
+
+def tri(side=6,alt=3):
+    pointA = cu_lo(CD1)
+    A = (pointA.lat, pointA.lon)
+    B = new_coords(A, side, 90)
+    goto(CD3, B[0], B[1], alt)
+    set_yaw(MCU,0)
+    set_yaw(CD1,0)
+    set_yaw(CD2,0)
+    set_yaw(CD3,0)
 
 def main():
 
