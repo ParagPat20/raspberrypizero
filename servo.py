fdcb733e14a3a847d250d95ddc7fc9316ef21d48
Parag Patil <parag.pat20@gmail.com>
2023-11-03 11:41:59 +0530

    Hello

--- servo.py
+++ servo.py
@@ -9,11 +9,11 @@ GPIO.setup(servo, GPIO.OUT)
 # Create a PWM object on the GPIO pin
 pwm = GPIO.PWM(servo, 50)
 pwm.start(0)
-angle = int(input("Enter Degrees: "))
-duty = (angle/18)+2.5
+
 try:
     while True:
-        
+        angle = int(input("Enter Degrees: "))
+        duty = (angle/18)+2.5
         # Rotate the servo motor to 0 degrees
         pwm.ChangeDutyCycle(duty)
 
