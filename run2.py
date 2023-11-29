688981e0dbcc4bbd85d3fc6c431981188fe58cae
ParagPat20 <parag.pat20@gmail.com>
2023-11-29 14:39:20 +0530

    New Commit

--- run2.py
+++ run2.py
@@ -97,7 +97,7 @@ def initialize_CD2():
             d1 = CD2
             d1_str = 'CD2'
             print("CD2 Connected")
-            threading.Thread(target=CD2.send_status, args=(status_port[0],)).start()
+            threading.Thread(target=CD2.send_status, args=(60003,)).start()
             CD2_initialized=True
         CD2.get_vehicle_state()
     except Exception as e:
@@ -111,7 +111,7 @@ def initialize_CD3():
             d1 = CD3
             d1_str = 'CD3'
             print("CD3 Connected")
-            threading.Thread(target=CD3.send_status, args=(status_port[1],)).start()
+            threading.Thread(target=CD3.send_status, args=(60004,)).start()
             CD3_initialized=True
         CD3.get_vehicle_state()
     except Exception as e:
