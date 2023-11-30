01628eef527899a7a3c76a612deb22884fdef49e
ParagPat20 <parag.pat20@gmail.com>
2023-11-30 11:57:10 +0530

    New Commit

--- testrp.py
+++ testrp.py
@@ -4,7 +4,7 @@ from drone import *
 log("Hello!")
 server = socket.socket()
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(('192.168.190.101', 12345))
+server.bind(('192.168.190.122', 12345))
 server.listen(100)
 conn ,addr = server.accept()
 while True:
