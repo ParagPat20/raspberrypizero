ef9d88e8723f1cb0bdb25096892c564cae148d3f
ParagPat20 <parag.pat20@gmail.com>
2023-12-04 15:27:58 +0530

    New Commit

--- drone.py
+++ drone.py
@@ -429,7 +429,7 @@ def log(msg, pc_host='192.168.170.101', port=61234):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cli:
             cli.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             try:
-                cli.connect((pc_host, port))
+                cli.connect(('192.168.170.101', 61234))
                 msg = str(msg)
                 cli.send(msg.encode())
                 print('Message sent')
