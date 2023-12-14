b8f0a5e034f468101d10ace7327196b4d42296f8
ParagPat20 <parag.pat20@gmail.com>
2023-12-14 13:56:23 +0530

    New Commit

--- drone.py
+++ drone.py
@@ -69,6 +69,7 @@ class Drone:
                 self.altitude = self.vehicle.location.global_relative_frame.alt
                 self.battery = self.vehicle.battery.voltage
                 log('{}Current altitude : {}m\nCurrent Battery {}V\n Alt Difference {}'.format(self.name, self.altitude, self.battery, self.altitude - self.posalt))
+                log("Wifi Connection : {}".format(str(self.is_wifi_connected())))
 
                 if not self.is_wifi_connected():
                     log("{}Wi-Fi connection lost! Initiating landing.".format(self.name))
