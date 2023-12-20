713313dc44afbd386eb4b68e8dd18064f5698e0a
ParagPat20 <parag.pat20@gmail.com>
2023-12-20 14:59:04 +0530

    New Commit

--- drone.py
+++ drone.py
@@ -50,9 +50,9 @@ class Drone:
         self.posalt = 2
         self.in_air = False
         self.no_vel_cmds = True
-        self.pid_velx = {'P': 1, 'I': 0.1, 'D': 0.1}
-        self.pid_vely = {'P': 1, 'I': 0.1, 'D': 0.1}
-        self.pid_velz = {'P': 1, 'I': 0.1, 'D': 0.1}
+        self.pid_velx = {'P': 1, 'I': 0.0, 'D': 0.1}
+        self.pid_vely = {'P': 1, 'I': 0.0, 'D': 0.1}
+        self.pid_velz = {'P': 1.1, 'I': 0.0, 'D': 0.1}
         self.prev_error_velx = 0.0
         self.prev_error_vely = 0.0
         self.prev_error_velz = 0.0
@@ -107,7 +107,7 @@ class Drone:
                         pid_output_velz = 2
                     log("VelocityZ PID Output : {}".format(pid_output_velz))
 
-                    self.send_ned_velocity_drone(pid_output_velx, pid_output_vely, -pid_output_velz)
+                    self.send_ned_velocity_drone(pid_output_velx, pid_output_vely, pid_output_velz)
 
             if not self.alt_ach:
                 if abs(self.altitude - self.posalt) > 0.1 and self.no_vel_cmds:
