ff1fe6bdeb84fbc27fc2cc61800796cda483bd03
ParagPat20 <parag.pat20@gmail.com>
2023-12-23 13:43:35 +0530

    New Commit

--- drone.py
+++ drone.py
@@ -296,7 +296,7 @@ class Drone:
             msg = self.vehicle.message_factory.set_position_target_local_ned_encode(
                 0,  # time_boot_ms (not used)
                 0, 0,  # target system, target component
-                mavutil.mavlink.MAV_FRAME_LOCAL_OFFSET_NED,  # frame
+                mavutil.mavlink.MAV_FRAME_LOCAL_NED,  # frame
                 0b0000111111111000,  # type_mask (only positions enabled)
                 disx, disy, disz,  # x, y, z positions (not used)
                 0, 0, 0,  # x, y, z velocity in m/s
