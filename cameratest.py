92edcc9de553f548abc092edcb34a2df90815cca
ParagPat20 <parag.pat20@gmail.com>
2024-03-30 11:25:25 +0530

    New Commit

--- cameratest.py
+++ cameratest.py
@@ -1,21 +1,18 @@
 import zmq
 import picamera
 import io
-import time
 
 context = zmq.Context()
-socket = context.socket(zmq.PUB)
+socket = context.socket(zmq.REQ)
 socket.bind("tcp://*:5555")
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
-    camera.framerate = 10  # Lowering the framerate to reduce data size
+    camera.framerate = 30
     stream = io.BytesIO()
-    
     for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
-        socket.send(stream.getvalue(), flags=zmq.SNDMORE)  # Sending frame
-        socket.send_string('latest', flags=0)  # Sending flag
-        
-        time.sleep(0.1)  # Wait for a short time to reduce the rate of frames sent
+        socket.send(stream.getvalue())
         stream.seek(0)
         stream.truncate()
+        
+        socket.recv_string()
--- laptopfeedtest.py
+++ laptopfeedtest.py
@@ -3,17 +3,15 @@ import cv2
 import numpy as np
 
 context = zmq.Context()
-socket = context.socket(zmq.SUB)
+socket = context.socket(zmq.REP)
 socket.connect("tcp://192.168.67.229:5555")
-socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
 while True:
     frame = socket.recv()
-    flag = socket.recv_string()
-    if flag == 'latest':
-        nparr = np.frombuffer(frame, np.uint8)
-        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        cv2.imshow('Frame', image)
+    socket.send_string('OK')
+    nparr = np.frombuffer(frame, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imshow('Frame', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
