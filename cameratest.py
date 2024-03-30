de5b4f254a78f9cad674d35d27b49050b3e2eca2
ParagPat20 <parag.pat20@gmail.com>
2024-03-30 10:10:06 +0530

    New Commit

--- cameratest.py
+++ cameratest.py
@@ -1,21 +1,23 @@
+import socket
 import picamera
+import io
 
-def check_camera():
-    try:
-        with picamera.PiCamera() as camera:
-            camera.resolution = (640, 480)
-            camera.start_preview()
-            input("Press Enter to capture an image...")
-            camera.capture('test_image.jpg')
-            print("Image captured successfully.")
-            camera.stop_preview()
-    except picamera.exc.PiCameraError as e:
-        print(f"An error occurred: {e}")
-        return False
-    return True
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('192.168.67.229', 8000))
+server_socket.listen(0)
 
-if __name__ == "__main__":
-    if check_camera():
-        print("Camera is working.")
-    else:
-        print("Camera is not working.")
+connection = server_socket.accept()[0].makefile('wb')
+
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        camera.framerate = 30
+        stream = io.BytesIO()
+        
+        for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+            connection.write(stream.getvalue())
+            stream.seek(0)
+            stream.truncate()
+finally:
+    connection.close()
+    server_socket.close()
