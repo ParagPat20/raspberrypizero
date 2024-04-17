8f4b2499a665634430a3ddb6a0fea10e52406242
ParagPat20 <parag.pat20@gmail.com>
2024-04-18 00:08:48 +0530

    New Commit

--- camtest.py
+++ camtest.py
@@ -3,7 +3,7 @@ import picamera
 try:
     with picamera.PiCamera() as camera:
         camera.resolution = (640, 480)
-        camera.capture('/home/pi/test_image.jpg')  # Capture an image and save it
+        camera.capture('/home/oxi/test_image.jpg')  # Capture an image and save it
         print("Image captured successfully. Camera is working.")
 except picamera.exc.PiCameraError as e:
     print("Error:", e)
