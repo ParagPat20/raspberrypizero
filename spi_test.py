5330aadd6d59783f2f72f973c0582b9e05773325
ParagPat20 <parag.pat20@gmail.com>
2024-12-17 00:57:29 +0530

    sd

--- spi_test.py
+++ spi_test.py
@@ -6,16 +6,23 @@ spi = spidev.SpiDev()
 spi.open(0, 0)  # Open SPI0 (bus 0, chip select 0)
 spi.max_speed_hz = 500000  # Set SPI clock speed (500kHz)
 
+# GPIO for chip select (SS)
+import RPi.GPIO as GPIO
+SS_PIN = 8  # GPIO pin for slave select
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SS_PIN, GPIO.OUT)
+GPIO.output(SS_PIN, GPIO.HIGH)
 
 # Function to send data
 def send_data(data):
+    GPIO.output(SS_PIN, GPIO.LOW)  # Select the slave
     response = spi.xfer2([data])  # Send data
+    GPIO.output(SS_PIN, GPIO.HIGH)  # Deselect the slave
     return response
 
 try:
     while True:
         data_to_send = 0x55  # Example data to send (hex 0x55)
-        spi.xfer2([0x01])
         print(f"Sending: {data_to_send}")
         response = send_data(data_to_send)
         print(f"Response: {response}")
@@ -25,3 +32,4 @@ except KeyboardInterrupt:
     print("Exiting...")
 finally:
     spi.close()
+    GPIO.cleanup()
