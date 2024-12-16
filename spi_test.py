a0fece73fbe8f1a148bf5e144be862d05bb0aa4f
ParagPat20 <parag.pat20@gmail.com>
2024-12-16 22:30:34 +0530

    h

--- spi_test.py
+++ spi_test.py
@@ -1,27 +1,12 @@
 import spidev
 import time
-
-# Initialize SPI
-spi = spidev.SpiDev()  # Create an SPI instance
-spi.open(0, 0)         # Open bus 0, device (CS) 0
-spi.max_speed_hz = 1000000  # 1 MHz
-spi.mode = 0  # SPI Mode 0 (CPOL=0, CPHA=0)
-
-def read_spi_data():
-    # Read data from the SPI bus
-    received_data = spi.xfer2([0x00])  # Send dummy byte to receive data
-    return received_data[0]  # Return received byte
-
-def main():
-    try:
-        while True:
-            data = read_spi_data()
-            print(f"Received: {data:08b}")  # Print received data as binary
-            time.sleep(0.1)  # Small delay
-    except KeyboardInterrupt:
-        print("Exiting...")
-    finally:
-        spi.close()  # Clean up and close SPI
-
-if __name__ == "__main__":
-    main()
+spi = spidev.SpiDev() # create spi object
+spi.open(0, 0) # open spi port 0, device (CS) 1
+try:
+ while True:
+    resp = spi.xfer2([0xAA]) # transfer one byte
+    time.sleep(0.1) # sleep for 0.1 seconds
+ #end while
+except KeyboardInterrupt: # Ctrl+C pressed, so…
+ spi.close() # … close the port before exit
+#end try
