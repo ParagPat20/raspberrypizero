2f6a370a0ce848d6343f97709ff334df956fbf80
ParagPat20 <parag.pat20@gmail.com>
2024-12-17 00:25:58 +0530

    vd

--- spi_test.py
+++ spi_test.py
@@ -1,23 +1,35 @@
 import spidev
 import time
 
-# Create an SPI object
+# Configure SPI
 spi = spidev.SpiDev()
-spi.open(0, 0)  # Open SPI bus 0, device (CS) 0
-spi.max_speed_hz = 50000  # Set the speed (50 kHz)
-spi.mode = 0b00  # Set SPI mode to 0
+spi.open(0, 0)  # Open SPI0 (bus 0, chip select 0)
+spi.max_speed_hz = 500000  # Set SPI clock speed (500kHz)
+
+# GPIO for chip select (SS)
+import RPi.GPIO as GPIO
+SS_PIN = 8  # GPIO pin for slave select
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SS_PIN, GPIO.OUT)
+GPIO.output(SS_PIN, GPIO.HIGH)
+
+# Function to send data
+def send_data(data):
+    GPIO.output(SS_PIN, GPIO.LOW)  # Select the slave
+    response = spi.xfer2([data])  # Send data
+    GPIO.output(SS_PIN, GPIO.HIGH)  # Deselect the slave
+    return response
 
 try:
     while True:
-        # Data to send
-        data = [0x31, 0x42, 0x43, 0x54]  # Example data
-        print(data)
-        response = spi.xfer2(data)  # Send data and receive response
-        print(data)
-        print("Sent:", data, "Received:", response)
-        time.sleep(1)  # Wait for 1 second
+        data_to_send = 0x55  # Example data to send (hex 0x55)
+        print(f"Sending: {data_to_send}")
+        response = send_data(data_to_send)
+        print(f"Response: {response}")
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
-    spi.close()  # Close the SPI connection
+    spi.close()
+    GPIO.cleanup()
