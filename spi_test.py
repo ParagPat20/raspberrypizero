26460e44d98b2f42188f81a090538a6aaad0fb0c
ParagPat20 <parag.pat20@gmail.com>
2024-12-17 15:40:49 +0530

    ddw

--- spi_test.py
+++ spi_test.py
@@ -3,30 +3,28 @@ import time
 
 # Create SPI instance
 spi = spidev.SpiDev()
-spi.open(0, 0)  # Open SPI bus 0, device (CS) 0
+spi.open(0, 0)  # Open SPI bus 0, device 0
 spi.max_speed_hz = 50000  # Set SPI clock speed
 spi.mode = 0b00  # SPI mode 0
 
-def send_message(message):
-    # Convert string to bytes and send over SPI
-    byte_message = [ord(char) for char in message]
-    spi.xfer2(byte_message)
-    print("Sent: ", message)
+BUFFER_SIZE = 8
 
-def read_message(length):
-    # Read the response from the SPI slave
-    response = spi.readbytes(length)
-    print("Received: ", "".join([chr(byte) for byte in response]))
+def transfer_data(send_buffer):
+    # Send data to the slave and receive the response
+    response = spi.xfer2(send_buffer)
+    return response
 
 try:
     while True:
-        # Send a message to the ESP32
-        send_message("Hello ESP32")
-        time.sleep(1)
+        # Send a sample message to the slave
+        send_buffer = [10, 20, 30, 40, 50, 60, 70, 80]
+        print("Sending: ", send_buffer)
 
-        # Read response from the ESP32 (assuming 32-byte response)
-        read_message(32)
-        time.sleep(1)
+        # Transfer data and print the response
+        response = transfer_data(send_buffer)
+        print("Received: ", response)
+
+        time.sleep(1)  # Optional: delay between transactions
 
 except KeyboardInterrupt:
     spi.close()
